6edd695af674c91916531a19514436b4ee22d716
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-13 14:22:50 +0100

    #9 - setup mirage on acceptance test

--- tests/acceptance/core-team-test.js
+++ tests/acceptance/core-team-test.js
@@ -1,9 +1,11 @@
 import { module, test } from "qunit";
 import { visit, currentURL } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
+import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
 
 module("Acceptance | core team", function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test("visiting /core-team or /", async function(assert) {
     await visit("/core-team");
