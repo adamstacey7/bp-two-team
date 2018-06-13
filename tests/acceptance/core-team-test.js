be0a1d01d83c5a97a3b9882771383cddbaa4e465
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-13 15:00:28 +0100

    #9 - change to single quotes

--- tests/acceptance/core-team-test.js
+++ tests/acceptance/core-team-test.js
@@ -1,26 +1,26 @@
-import { module, test } from "qunit";
-import { visit, currentURL } from "@ember/test-helpers";
-import { setupApplicationTest } from "ember-qunit";
-import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-module("Acceptance | core team", function(hooks) {
+module('Acceptance | core team', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test("visiting /core-team or /", async function(assert) {
-    await visit("/core-team");
-    assert.equal(currentURL(), "/core-team");
+  test('visiting /core-team or /', async function(assert) {
+    await visit('/core-team');
+    assert.equal(currentURL(), '/core-team');
 
-    await visit("/");
-    assert.equal(currentURL(), "/core-team");
+    await visit('/');
+    assert.equal(currentURL(), '/core-team');
   });
 
   test("should have the text 'Hello World!", async function(assert) {
-    await visit("/");
+    await visit('/');
     assert.equal(
-      this.element.querySelector(".core-team p").textContent,
-      "Hello World!",
-      "should display Hello World!"
+      this.element.querySelector('.core-team p').textContent,
+      'Hello World!',
+      'should display Hello World!'
     );
   });
 });
