741f843acc0bea428f93ae0009ba52749f569321
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-18 16:49:35 +0100

    #9 - use model data to show list items

--- app/models/member.js
+++ app/models/member.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  basicDetails: DS.belongsTo('basic-details'),
+  basicDetails: DS.belongsTo('basic-detail'),
   skills: DS.attr(),
   technologies: DS.attr(),
   industries: DS.attr(),
-  professionalExperiences: DS.hasMany('professional-experience'),
+  professionalExperiences: DS.hasMany('experience'),
   educations: DS.hasMany('education')
 });
