849ff20395bfa7ae7cb44ddaa5bfbd3b43c36c2a
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-18 18:26:13 +0100

    #12 - added UAT test for creating user

--- tests/acceptance/create-member-test.js
+++ tests/acceptance/create-member-test.js
@@ -1,10 +1,12 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | create member', function(hooks) {
   setupApplicationTest(hooks);
-
+  setupMirage(hooks);
+  
   test('visiting /member/create', async function(assert) {
     await visit('/member/create');
 
@@ -15,5 +17,16 @@ module('Acceptance | create member', function(hooks) {
     await visit('/member/create');
 
     assert.equal(this.element.querySelector('h2').textContent, 'Please create a member');
+  });
+
+  test('check user can submit first name and last name', async function(assert) {
+    await visit('/member/create');
+
+    await fillIn('input.first-name', 'Adam');
+    await fillIn('input.last-name', 'Smith');
+
+    await click('button.submit');
+
+    assert.equal(currentURL(), '/member/123');
   });
 });
