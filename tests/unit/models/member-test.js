ffd85aa07a6b1c3ac3c23fbdbea0708b306860ba
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-18 11:57:25 +0100

    #20 - adding in test for member model relations

--- tests/unit/models/member-test.js
+++ tests/unit/models/member-test.js
@@ -1,12 +1,27 @@
-import { moduleForModel, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import { get } from '@ember/object';
 
-moduleForModel('member', 'Unit | Model | member', {
-  // Specify the other units that are required for this test.
-  needs: []
-});
+module('member', 'Unit | Model | member', function(hooks) {
+  setupTest(hooks);
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+  test('should have educations relation', function(assert) {
+    const Member = this.owner.lookup('service:store').modelFor('member');
+
+    // lookup the relationship on the user model
+    const relationship = get(Member, 'relationshipsByName').get('educations');
+
+    assert.equal(relationship.key, 'educations', 'has relationship with education');
+    assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
+  });
+
+  test('should have experience relation', function(assert) {
+    const Member = this.owner.lookup('service:store').modelFor('member');
+
+    // lookup the relationship on the user model
+    const relationship = get(Member, 'relationshipsByName').get('experiences');
+
+    assert.equal(relationship.key, 'experiences', 'has relationship with experiences');
+    assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
+  });
 });
