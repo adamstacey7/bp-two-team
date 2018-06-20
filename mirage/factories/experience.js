b42104774ed1e8fa422286c92a96cc27020ffe67
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-20 12:04:09 +0100

    #12 - removed bad JSON data

--- mirage/factories/experience.js
+++ mirage/factories/experience.js
@@ -1,26 +1,5 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
-//   const experienceData = [
-//     {
-//       "type": "experience",
-//       "id": "1",
-//       "attributes": {
-//         "title": "Porche",
-//         "start-date": "1529061341148",
-//         "end-date": "1529061347803"
-//       }
-//     },
-//     {
-//       "type": "experience",
-//       "id": "2",
-//       "attributes": {
-//         "title": "Landrover",
-//         "start-date": "1529061341148",
-//         "end-date": "1529061347803"
-//       }
-//     }
-//   ];
-
 export default Factory.extend({
   id(i) { return `${i}`; },
   title: faker.list.cycle('Porche', 'Landrover'),
--- mirage/factories/member.js
+++ mirage/factories/member.js
@@ -1,50 +1,5 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
-// const memberData = [
-//     {
-//       "type": "member",
-//       "id": "1",
-//       "attributes": {
-//         "first-name": "Tom",
-//         "last-name": "Smith",
-//       },
-//       "relationships": {
-//         "experiences": {
-//           "data": [
-//             {
-//               "type": "experience",
-//               "id": "1"
-//             },
-//             {
-//               "type": "experience",
-//               "id": "2"
-//             }          
-//           ]
-//         },
-//         "educations": {
-//           "data": [
-//             {
-//               "type": "education",
-//               "id": "1"
-//             },
-//             {
-//               "type": "education",
-//               "id": "2"
-//             }          
-//           ]          
-//         }
-//       },
-//     },
-//     {
-//       "type": "member",
-//       "id": "2",
-//       "attributes": {
-//         "first-name": "Mark",
-//         "last-name": "Peterson"
-//       }
-//     }
-//   ];
-
 export default Factory.extend({
   id(i) { return `${i}`; },
   firstName: faker.list.cycle('Tom', 'Mark'),
