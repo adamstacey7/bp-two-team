997d5ad2967575ac482e5ef72d1e94f837378e37
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-20 14:15:04 +0100

    #9 - add new files

---
+++ app/routes/member/show.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model: function(params) {
+    return this.get('store').findRecord('member', params.member_id);
+  },
+  actions: {
+    error: function(reason) {
+      if (reason.errors[0].status == '404') {
+        this.transitionTo('not-found', ' ');
+      }
+    }
+  }
+});
