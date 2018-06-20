bd5c1339746dec0eaa1c8529cb05283dc1b8f636
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-20 11:54:48 +0100

    #12 - got educations working

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,8 +1,9 @@
 export default function(server) {
-  // server.createList('education', 2);
+  server.createList('education', 2);
   server.createList('experience', 2);
 
   server.createList('member', 2, {
-    experiences: server.createList('experience', 2)
+    experiences: server.createList('experience', 2),
+    educations: server.createList('education', 2)
   });
 }
--- mirage/serializers/member.js
+++ mirage/serializers/member.js
@@ -1,12 +1,10 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
-    // embed: true,
-    // alwaysIncludeLinkageData: true,
-    // serializeIds: 'always',
   include() {
     return [
-      'experiences'
+      'experiences',
+      'educations'
     ]
   }
 });
