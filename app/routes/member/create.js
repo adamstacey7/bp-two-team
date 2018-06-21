935fd2cd83bfbb2c238abd577abf2d2072ef2309
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-21 15:48:57 +0100

    #12 - fixed merge bug

--- app/routes/member/create.js
+++ app/routes/member/create.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  application: service(),
+  init() {
+    this._super(...arguments);
+    this.get('application').setHeaderTitle('Create Member');
+  },    
 });
