3f16ea229f45825e5d0e28e522d6ceb5fb49c935
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-26 17:28:32 +0100

    #14 - add unit test for deleteMember action in the route

--- tests/unit/routes/core-team-test.js
+++ tests/unit/routes/core-team-test.js
@@ -4,8 +4,20 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Route | core-team', function(hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
+  test('should call delete record and the call save for a model passed in on method deleteMember', function(assert) {
+    assert.expect(4);
     const route = this.owner.lookup('route:core-team');
-    assert.ok(route);
+
+    const member = {
+      deleteRecord() {
+        assert.ok(true, 'should call deleteRecord');
+      },
+      save() {
+        assert.ok(true, 'should call save');
+      }
+    };
+
+    route._deleteMember(member);
+    route.send('deleteMember', member);
   });
 });
