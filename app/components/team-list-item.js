5d4bed64d2bee07164a149456ff7cd019bfdd2d3
Aidan Freeman <aidan.freeman@capgemini.com>
2018-06-27 10:45:03 +0100

    #14 - make changes requested

--- app/components/team-list-item.js
+++ app/components/team-list-item.js
@@ -3,7 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   actions: {
     deleteMember(member) {
-      console.log(this.get('deleteMember'));
       this.get('deleteMember')(member);
     }
   }
