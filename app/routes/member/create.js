254dd2c33e6b6b68deb0b5d99774eef916e6f9e6
Adam Smith <adamsmit@Adams-MacBook-Pro-6.local>
2018-06-27 14:11:47 +0100

    #13 - refactored create route to conform to edit code

--- app/routes/member/create.js
+++ app/routes/member/create.js
@@ -1,36 +1,24 @@
 import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
   application: service(),
+  model() {
+    return this.get('store').createRecord('member', {
+      experiences: [this.get('store').createRecord('experience', {})],
+      educations: [this.get('store').createRecord('education', {})]
+    });
+  },
   init() {
     this._super(...arguments);
     this.get('application').setHeaderTitle('Create Member');
   },
-  _createData(formData) {
-    const member = this.get('store').createRecord('member', {
-      firstName: formData.firstName,
-      lastName: formData.lastName
-    });
+  _createData() {
+    const model = this.modelFor(this.routeName);
 
-    return member.save().then(savedMember => {
-      const experience = this.get('store').createRecord('experience', {
-        details: formData.details,
-        industry: formData.industry,
-        member: savedMember
-      });
-
-      const education = this.get('store').createRecord('education', {
-        title: formData.title,
-        grade: formData.grade,
-        member: savedMember
-      });
-
-      const p = RSVP.Promise.all([experience.save(), education.save()]);
-
-      return p.then(() => this.transitionTo(`/member/${member.get('id')}`));
-    });
+    return model
+      .save()
+      .then(() => this.transitionTo(`/member/${model.get('id')}`));
   },
   actions: {
     submitThree(formData) {
--- tests/unit/routes/member/create-test.js
+++ tests/unit/routes/member/create-test.js
@@ -1,41 +1,28 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import sinon from 'sinon';
 
 module('Unit | Route | route:member/create', function(hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    const route = this.owner.lookup('route:member/create');
-    assert.ok(route);
-  });
-
   test('it calls the create record method 3 times', function(assert) {
     const route = this.owner.lookup('route:member/create');
 
-    const myCreateRecordSpy = sinon.spy(() => ({
+    const myGetMock = () => ({
       save() {
+        assert.ok(true, 'should call save');
         return Promise.resolve();
       },
       get() {
+        assert.ok(true, 'should call id');
         return '5';
       }
-    }));
-
-    const myGetMock = () => ({
-      createRecord: myCreateRecordSpy
     });
 
-    route.get = myGetMock;
-    route.transitionTo = () => {};
-    route
-      ._createData({
-        firstName: 'test first name',
-        lastName: 'test last name'
-      })
-      .then(() => {
-        assert.equal(myCreateRecordSpy.callCount, 3);
-        assert.ok(route);
-      });
+    route.modelFor = myGetMock;
+    route.transitionTo = () => {
+      assert.ok(true, 'should call transition');
+    };
+
+    return route._createData();
   });
 });
